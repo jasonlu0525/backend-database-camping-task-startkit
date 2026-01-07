842d247b3db04153fe8c98f2e39635c076e8c40c
JasonLu <s099113302@gmail.com>
2026-01-07 21:32:04 +0800

    fix : 修改 1-1 寫入的 email

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -16,7 +16,7 @@ INSERT INTO
     "USER" (name, email, role)
 VALUES
     ('李燕容', 'lee2000@hexschooltest.io', 'USER'),
-    ('王小明', ' wXlTq@hexschooltest.io', 'USER'),
+    ('王小明', 'wXlTq@hexschooltest.io', 'USER'),
     ('肌肉棒子', 'muscle@hexschooltest.io', 'USER'),
     ('好野人', 'richman@hexschooltest.io', 'USER'),
     ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
