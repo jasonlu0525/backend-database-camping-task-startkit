2ae3fd57ab55a233bb8da5d2429e7c38583f2e4e
JasonLu <s099113302@gmail.com>
2026-01-07 22:51:03 +0800

    fix : 修復 ; 錯誤

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -618,11 +618,12 @@ WHERE
                 WHERE
                     email = 'lee2000@hexschooltest.io'
             )
-    )
-    -- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
-    -- 1. 預約人設為`王小明`
-    -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
-    -- 3. 狀態`status` 設定為即將授課
+    );
+
+-- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
+-- 1. 預約人設為`王小明`
+-- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
+-- 3. 狀態`status` 設定為即將授課
 insert into
     "COURSE_BOOKING" (user_id, course_id, booking_at, status)
 values
