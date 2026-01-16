392d7873aa1faca9b5e6374d0444a6f8ffedbd74
JasonLu <s099113302@gmail.com>
2026-01-16 18:27:48 +0800

    feat : 完成第五題

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -705,7 +705,42 @@ WHERE
     AND status = '即將授課';
 
 -- 5-6. 查詢：計算用戶王小明的購買堂數，顯示須包含以下欄位： user_id , total。 (需使用到 SUM 函式與 Group By)
+SELECT
+    user_id,
+    SUM(purchased_credits) AS total
+FROM
+    "CREDIT_PURCHASE"
+WHERE
+    user_id = (
+        SELECT
+            id
+        FROM
+            "USER"
+        WHERE
+            email = 'wXlTq@hexschooltest.io'
+    )
+Group BY
+    user_id;
+
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
+SELECT
+    user_id,
+    Count(*) AS total
+FROM
+    "COURSE_BOOKING"
+WHERE
+    user_id = (
+        SELECT
+            id
+        FROM
+            "USER"
+        WHERE
+            email = 'wXlTq@hexschooltest.io'
+    )
+    AND status NOT IN ('已取消')
+Group BY
+    user_id;
+
 -- 5-8. [挑戰題] 查詢：請在一次查詢中，計算用戶王小明的剩餘可用堂數，顯示須包含以下欄位： user_id , remaining_credit
 -- 提示：
 -- select ("CREDIT_PURCHASE".total_credit - "COURSE_BOOKING".used_credit) as remaining_credit, ...
